c564e72d14a1d1a85ef366c1244f56dd811c0ff4
Iron_noob_73 <1192869181@qq.com>
2022-02-08 14:18:48 +0800

    20220208DU1

--- src/main/java/idv/hailelectronic/dimensiontraveler/DimensionTraveler.java
+++ src/main/java/idv/hailelectronic/dimensiontraveler/DimensionTraveler.java
@@ -1,5 +1,8 @@
 package idv.hailelectronic.dimensiontraveler;
 
+import idv.hailelectronic.dimensiontraveler.items.DenyUse;
+import io.github.thebusybiscuit.slimefun4.api.researches.Research;
+import io.github.thebusybiscuit.slimefun4.implementation.SlimefunItems;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
@@ -20,45 +23,40 @@ public class DimensionTraveler extends JavaPlugin implements SlimefunAddon {
         // Read something from your config.yml
         Config cfg = new Config(this);
 
-        if (cfg.getBoolean("options.auto-update")) {
+        //if (cfg.getBoolean("options.auto-update")) {
             // You could start an Auto-Updater for example
-        }
-
-        /*
-         * 1. Creating a new Category
-         * This Category will use the following ItemStack
-         */
-        ItemStack itemGroupItem = new CustomItemStack(Material.DIAMOND, "&4Addon Category", "", "&a> Click to open");
-
-        // Give your Category a unique id.
-        NamespacedKey itemGroupId = new NamespacedKey(this, "addon_category");
+        //}
+        //Category
+        ItemStack itemGroupItem = new CustomItemStack(Material.BASALT, "&b&k|||&r&b维度旅者&k|||", "");
+        NamespacedKey itemGroupId = new NamespacedKey(this, "dimension_traveler");
+        //Item
+        //  Raw Cast Slate
         ItemGroup itemGroup = new ItemGroup(itemGroupId, itemGroupItem);
-
-        /*
-         * 2. Create a new SlimefunItemStack
-         * This class has many constructors, it is very important
-         * that you give each item a unique id.
-         */
-        SlimefunItemStack slimefunItem = new SlimefunItemStack("COOL_DIAMOND", Material.DIAMOND, "&4Cool Diamond", "&c+20% Coolness");
-
-        /*
-         * 3. Creating a Recipe
-         * The Recipe is an ItemStack Array with a length of 9.
-         * It represents a Shaped Recipe in a 3x3 crafting grid.
-         * The machine in which this recipe is crafted in is specified
-         * further down as the RecipeType.
-         */
-        ItemStack[] recipe = { new ItemStack(Material.EMERALD), null, new ItemStack(Material.EMERALD), null, new ItemStack(Material.DIAMOND), null, new ItemStack(Material.EMERALD), null, new ItemStack(Material.EMERALD) };
-
-        /*
-         * 4. Registering the Item
-         * Now you just have to register the item.
-         * RecipeType.ENHANCED_CRAFTING_TABLE refers to the machine in
-         * which this item is crafted in.
-         * Recipe Types from Slimefun itself will automatically add the recipe to that machine.
-         */
-        SlimefunItem item = new SlimefunItem(itemGroup, slimefunItem, RecipeType.ENHANCED_CRAFTING_TABLE, recipe);
-        item.register(this);
+        SlimefunItemStack sfItemRawCastSlate = new SlimefunItemStack("RAW_CAST_SLATE", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvY2ZlNDgwNmQxODVhYzA4MDNjMjMyNjE5NDFiM2Q0N2M5YzA2NWYyZDJhYmQyYzY2YjFkN2EwYTdkYTM2NjQzMCJ9fX0=", "&7未烧制的铸石", "&6Basic material","&8&ndimension_traveler:raw_cast_slate");
+        ItemStack[] recipeRawCastSlate = { new ItemStack(Material.BASALT), new ItemStack(Material.BASALT), new ItemStack(Material.BASALT), SlimefunItems.IRON_DUST, new ItemStack(Material.DIAMOND), SlimefunItems.IRON_DUST, new ItemStack(Material.BASALT), new ItemStack(Material.BASALT), new ItemStack(Material.BASALT) };
+        SlimefunItem itemRawCastSlate = new SlimefunItem(itemGroup, sfItemRawCastSlate, RecipeType.ENHANCED_CRAFTING_TABLE, recipeRawCastSlate,new SlimefunItemStack(sfItemRawCastSlate,8));
+        itemRawCastSlate.register(this);
+        //  Cast Slate
+        SlimefunItemStack sfItemCastSlate = new SlimefunItemStack("CAST_SLATE", Material.DRIED_KELP, "&7铸石板", "&6Basic material","&8&ndimension_traveler:cast_slate");
+        ItemStack[] recipeCastSlate = { getItem("RAW_CAST_SLATE")};
+        DenyUse itemCastSlate = new DenyUse(itemGroup, sfItemCastSlate, RecipeType.SMELTERY, recipeCastSlate);
+        itemCastSlate.register(this);
+        //  Dimension Gem
+        SlimefunItemStack sfItemDimensionGem = new SlimefunItemStack("DIMENSION_GEM", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvN2I5NTFmZWQ2YTdiMmNiYzIwMzY5MTZkZWM3YTQ2YzRhNTY0ODE1NjRkMTRmOTQ1YjZlYmMwMzM4Mjc2NmQzYiJ9fX0=", "&9维度宝石", "&6Basic material","&8&ndimension_traveler:dimension_gem");
+        ItemStack[] recipeDimensionGem = { null,new ItemStack(Material.ENDER_PEARL),null,new ItemStack(Material.ENDER_PEARL),new ItemStack(Material.DIAMOND),new ItemStack(Material.ENDER_PEARL),null,new ItemStack(Material.ENDER_PEARL),null };
+        SlimefunItem itemDimensionGem = new SlimefunItem(itemGroup, sfItemDimensionGem, RecipeType.ENHANCED_CRAFTING_TABLE, recipeDimensionGem);
+        itemDimensionGem.register(this);
+        //  Portal Controller
+        SlimefunItemStack sfItemPortalController = new SlimefunItemStack("PORTAL_CONTROLLER", Material.LODESTONE, "&e传送门控制器", "&6Functional Block","&8&ndimension_traveler:portal_controller");
+        ItemStack[] recipePortalController = { getItem("CAST_SLATE"),getItem("CAST_SLATE"),getItem("CAST_SLATE"),getItem("CAST_SLATE"),getItem("DIMENSION_GEM"),getItem("CAST_SLATE"),getItem("CAST_SLATE"),getItem("CAST_SLATE"),getItem("CAST_SLATE")};
+        SlimefunItem itemPortalController = new SlimefunItem(itemGroup, sfItemPortalController, RecipeType.ENHANCED_CRAFTING_TABLE, recipePortalController);
+        itemPortalController.register(this);
+        //Research
+        //  000
+        NamespacedKey beforeEnter =  new NamespacedKey( this , " before_enter " );
+        Research research000 =  new Research(beforeEnter, 730516000 , " Before enter " , 15 );
+        research000.addItems(itemRawCastSlate,itemCastSlate,itemDimensionGem,itemPortalController);
+        research000.register();
     }
 
     @Override
@@ -80,5 +78,15 @@ public class DimensionTraveler extends JavaPlugin implements SlimefunAddon {
          */
         return this;
     }
+    /*Copy from ExoticGarden:
+     *https://github.com/TheBusyBiscuit/ExoticGarden/blob/master/src/main/java/io/github/thebusybiscuit/exoticgarden/ExoticGarden.java
+     * Line:332~336
+     */
+    private static ItemStack getItem(String id) {
+        SlimefunItem item = SlimefunItem.getById(id);
+        return item != null ? item.getItem() : null;
+    }
+
+
 
 }
---
+++ src/main/java/idv/hailelectronic/dimensiontraveler/items/DenyUse.java
@@ -0,0 +1,24 @@
+package idv.hailelectronic.dimensiontraveler.items;
+
+import io.github.thebusybiscuit.slimefun4.api.events.PlayerRightClickEvent;
+import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
+import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
+import io.github.thebusybiscuit.slimefun4.core.handlers.ItemUseHandler;
+import org.bukkit.inventory.ItemStack;
+
+public class DenyUse extends SlimefunItem {
+    public DenyUse(ItemGroup itemGroup, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
+        super(itemGroup, item, recipeType, recipe);
+    }
+    @Override
+    public void preRegister() {
+        ItemUseHandler itemUseHandler = this::onItemRightClick;
+        addItemHandler(itemUseHandler);
+    }
+
+    private void onItemRightClick(PlayerRightClickEvent event) {
+        event.cancel();
+    }
+}
